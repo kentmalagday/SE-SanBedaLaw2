54de92d07ca19e5645e6030219f36834ed6a9815
Lance <ronquillolance@gmail.com>
2022-06-15 10:33:58 +0800

    view-repo and access-reqs connected to db

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from admin import admin
 
 app = Flask(__name__)
 app.register_blueprint(admin, url_prefix='/admin')
+app.secret_key = "SE-SanBedaLaw"
 
 #routes to user/client side
 @app.route('/', methods=["POST", "GET"])
@@ -49,14 +50,15 @@ def signUpPage():
             auth.send_email_verification(new_user['idToken'])
             data = {"fullName" : fullName,
                     "institution" : institution,
-                    "email" : email,
-                    "admin" : False}
+                    "email" : email
+                    }
             db.child('users').child(new_user['localId']).set(data)                                     #add formatted data to Realtime DB
         except:
             existing_account = "Error in sign-up"                                           #catch error if email is used already
             print(existing_account)
             return render_template('/user-page/user_signup.html')
-        return redirect(url_for("signInPage"))
+        else:
+            return redirect(url_for("signInPage"))
     else:
         return render_template('/user-page/user_signup.html')
 
@@ -66,15 +68,14 @@ def signInPage():
         email = request.form["email"]
         password = request.form["password"]
         try:
-            login = auth.sign_in_with_email_and_password(email, password)
-            print(login['idToken'])
-            user = db.child('users').child(login['localId']).child('admin').get()
+            user = auth.sign_in_with_email_and_password(email, password)
+            userData = db.child('users').child(user['localId']).get()
         except:
             invalid_cred = "Invalid credentials"
             print(invalid_cred)
             return render_template('/user-page/user_signin.html')
         else:
-            if user.val() is True:
+            if userData.val() is None:
                 invalid_cred = "Invalid credentials"
                 return render_template('/user-page/user_signin.html')
             return redirect(url_for('indexPage'))
@@ -105,8 +106,6 @@ def contactPage():
 def aboutUsPage():
     return render_template('/user-page/aboutus.html')
 
-
-#routes to admin-side
 
 if __name__ == "__main__":
     app.run(debug=True)
